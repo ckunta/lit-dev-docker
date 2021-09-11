9eec6f48f0a8973b98d425d3e24b15dc5e712471
ckunta <23427639+ckunta@users.noreply.github.com>
2021-09-11 08:13:06 -0600

    make entrypoint generic to run c3do assuming it is mounted in SourceCode

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$1" = 'default' ]; then
     # do default thing here
     echo "Running default"
-    cd ~/SourceCode/c3do-classroom-in-docker/c3do-classroom
+    cd ~/SourceCode/c3do-classroom
     exec npm run start
 else
     echo "Running user supplied arg $1"
