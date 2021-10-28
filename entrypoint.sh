7ace61466983d7d49653f0e30fa8bb755aa71693
ckunta <23427639+ckunta@users.noreply.github.com>
2021-10-27 22:00:40 -0600

    add init argument for first time; assume code is checkout into ~/SourceCode

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,14 @@ set -e
 if [ "$1" = 'default' ]; then
     # do default thing here
     echo "Running default"
-    cd ~/SourceCode/c3do-classroom
+    cd ~/SourceCode
     exec npm run start
+elif [ "$1" = 'init' ]; then
+# setting up dev environment for the first time
+	 echo "Running init"
+	 mv ~/node_modules ~/SourceCode
 else
     echo "Running user supplied arg $1"
-    cd ~/SourceCode/$1
-    exec npm run start
+    cd ~/SourceCode
+    exec npm run $1
 fi
